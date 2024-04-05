9353abe2babfc6cbdd96e74743714e99df722a7e
Yongwook Choi <dyddnr25@gmail.com>
2024-04-06 00:54:58 +0900

    VarEx

---
+++ inflearn/java/java-start/src/variable/ex/VarEx1Q.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx1Q {
+    public static void main(String[] args) {
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+}
+/*다음 코드에 반복해서 나오는 숫자 4 , 3 을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 num1 , num2 를 사용하
+도록 변경해보세요.*/
